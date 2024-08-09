668e696520899b226cf80e65fb6cef3c52c8ad8f
ZsoltGaal <gaal.zsolt.19f@szlgbp.hu>
2024-08-09 20:12:35 +0200

    Update SECRET_KEY configuration in settings.py, it is read from local settings

--- restapi/restapi/settings.py
+++ restapi/restapi/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&1x9(49qtcuz*y*#9bx9q5s7d21$k!qia-h566j)&xj5(to9#p'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
